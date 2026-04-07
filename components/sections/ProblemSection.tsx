011aa075b95a1f4aaf4325be84201f5c2164f145
Dan George <dangeorge@Dans-MacBook-Air.local>
2026-04-07 01:23:39 +0100

    Add empty section components

---
+++ components/sections/ProblemSection.tsx
@@ -0,0 +1,7 @@
+export const ProblemSection = () => {
+  return (
+    <section>
+      ProblemSection
+    </section>
+  );
+};
---
+++ components/sections/SocialProofSection.tsx
@@ -0,0 +1,7 @@
+export const SocialProofSection = () => {
+  return (
+    <section>
+      SocialProofSection
+    </section>
+  );
+};
